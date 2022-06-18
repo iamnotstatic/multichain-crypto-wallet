cdaf55c7c04719ab8efab5d13a2b45df9d54c73e
iamnotstatic <staticdev20046@gmail.com>
2022-06-18 22:52:38 +0400

    Made solana provider config rpcUrl param optional

--- src/common/utils/solana.ts
+++ src/common/utils/solana.ts
@@ -1,7 +1,7 @@
 import * as solanaWeb3 from '@solana/web3.js';
 
-const provider = (rpcUrl: string) => {
-  return new solanaWeb3.Connection(rpcUrl);
+const provider = (rpcUrl?: string) => {
+  return new solanaWeb3.Connection(rpcUrl as string);
 };
 
 export default provider;
