c6f407e335d61e84e8901e449d1d7e3267bcfd92
iamnotstatic <staticdev20046@gmail.com>
2022-07-03 19:53:07 +0400

    Updated bitcoin tests to sync with changes made on Bitcoin Helper

--- test/wallet.bitcoin.test.ts
+++ test/wallet.bitcoin.test.ts
@@ -17,21 +17,26 @@ describe('MultichainCryptoWallet Bitcoin tests', () => {
   it('generateWalletFromMnemonic', async () => {
     const wallet = await generateWalletFromMnemonic({
       mnemonic:
-        'base dry mango subject neither labor portion weekend range couple right document',
+        'excess quit spot inspire stereo scrap cave wife narrow era pizza typical',
       derivationPath: "m/44'/0'/0'/0/0", // Leave empty to use default derivation path
       network: 'bitcoin', // 'bitcoin' or 'bitcoin-testnet'
     });
 
-    expect(typeof wallet).toBe('object');
+    expect(wallet.address).toBe('1NV8FPKDW1hxJFxc2dNVZDAp7iCqxCLeFu');
+    expect(wallet.privateKey).toBe(
+      'KxqTGtCMnX6oL9rxynDKCRJXt64Gm5ame4AEQcYncFhSSUxFBkeJ'
+    );
+    expect(wallet.mnemonic).toBe(
+      'excess quit spot inspire stereo scrap cave wife narrow era pizza typical'
+    );
   });
 
   it('getAddressFromPrivateKey', async () => {
     const data = await getAddressFromPrivateKey({
-      privateKey:
-        'L4gDoFUt4eJg7aK9nA3dop7xneeHUWA9wMFrWh7mEeV998YHCEio',
+      privateKey: 'KxqTGtCMnX6oL9rxynDKCRJXt64Gm5ame4AEQcYncFhSSUxFBkeJ',
       network: 'bitcoin', // 'bitcoin' or 'bitcoin-testnet'
     });
 
-    expect(data.address).toBe('3FvEGh9oB3i7GjhVUu4QpqgTKgPvLMCq5z');
+    expect(data.address).toBe('1NV8FPKDW1hxJFxc2dNVZDAp7iCqxCLeFu');
   });
 });
