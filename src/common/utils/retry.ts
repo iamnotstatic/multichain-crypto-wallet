ccaaa2927dd800261716db79ebc67cf2c30a990c
iamnotstatic <staticdev20046@gmail.com>
2022-07-26 02:19:35 +0400

    Fixed lint error on retry

--- src/common/utils/retry.ts
+++ src/common/utils/retry.ts
@@ -1,46 +1,46 @@
 export const sleep = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
+  new Promise(resolve => setTimeout(resolve, ms));
 
 export const extractError = (error: any): string => {
-    if (error && typeof error === "object") {
-        if (error.response) {
-            return extractError(error.response);
-        }
-        if (error.error) {
-            return extractError(error.error);
-        }
-        if (error.message) {
-            return extractError(error.message);
-        }
-        if (error.data) {
-            return extractError(error.data);
-        }
-        if (error.context) {
-            return extractError(error.context);
-        }
-        if (error.statusText) {
-            return extractError(error.statusText);
-        }
-        try {
-            return JSON.stringify(error);
-        } catch (error: any) {
-            // Ignore JSON error
-        }
+  if (error && typeof error === 'object') {
+    if (error.response) {
+      return extractError(error.response);
     }
-    // Remove `Error: ` prefix.
+    if (error.error) {
+      return extractError(error.error);
+    }
+    if (error.message) {
+      return extractError(error.message);
+    }
+    if (error.data) {
+      return extractError(error.data);
+    }
+    if (error.context) {
+      return extractError(error.context);
+    }
+    if (error.statusText) {
+      return extractError(error.statusText);
+    }
     try {
-        if (typeof error === "string") {
-            if (error.slice(0, 7).toLowerCase() === "Error: ") {
-                // tslint:disable-next-line: no-parameter-reassignment
-                error = error.slice(7);
-            }
-            return error;
-        }
-        return JSON.stringify(error);
-    } catch (error: any) {
-        // Ignore JSON error
+      return JSON.stringify(error);
+    } catch (error) {
+      // Ignore JSON error
     }
-    return String(error);
+  }
+  // Remove `Error: ` prefix.
+  try {
+    if (typeof error === 'string') {
+      if (error.slice(0, 7).toLowerCase() === 'Error: ') {
+        // tslint:disable-next-line: no-parameter-reassignment
+        error = error.slice(7);
+      }
+      return error;
+    }
+    return JSON.stringify(error);
+  } catch (error) {
+    // Ignore JSON error
+  }
+  return String(error);
 };
 
 // export const onlyMainnet = <T>(x: any, testnet: boolean) =>
@@ -49,44 +49,43 @@ export const extractError = (error: any): string => {
 //     testnet ? x : undefined;
 
 export const fallback = async <T>(
-    fallbacks: Array<undefined | (() => Promise<T>)>
+  fallbacks: Array<undefined | (() => Promise<T>)>
 ): Promise<T> => {
-    let firstError: Error | undefined;
-    for (const fn of fallbacks) {
-        if (!fn) {
-            continue;
-        }
-        try {
-           
-            return await fn();
-        } catch (error: any) {
-            firstError = firstError || error;
-        }
+  let firstError: Error | undefined;
+  for (const fn of fallbacks) {
+    if (!fn) {
+      continue;
     }
-    throw firstError || new Error("No result returned");
+    try {
+      return await fn();
+    } catch (error) {
+      firstError = firstError || (error as Error);
+    }
+  }
+  throw firstError || new Error('No result returned');
 };
 
 export const retryNTimes = async <T>(
-    fnCall: () => Promise<T>,
-    retries: number
+  fnCall: () => Promise<T>,
+  retries: number
 ) => {
-    let returnError;
-    for (let i = 0; i < retries; i++) {
-        try {
-            return await fnCall();
-        } catch (error: any) {
-            if (String(error).match(/timeout of .* exceeded/)) {
-                returnError = error;
-            } else {
-                const errorMessage = extractError(error);
-                if (errorMessage) {
-                    // tslint:disable-next-line: no-object-mutation
-                    error.message += ` (${errorMessage})`;
-                }
-                throw error;
-            }
+  let returnError;
+  for (let i = 0; i < retries; i++) {
+    try {
+      return await fnCall();
+    } catch (error) {
+      if (String(error).match(/timeout of .* exceeded/)) {
+        returnError = error;
+      } else {
+        const errorMessage = extractError(error);
+        if (errorMessage) {
+          // tslint:disable-next-line: no-object-mutation 
+          error.message += ` (${errorMessage})`;
         }
-        await sleep(500);
+        throw error;
+      }
     }
-    throw returnError;
+    await sleep(500);
+  }
+  throw returnError;
 };
