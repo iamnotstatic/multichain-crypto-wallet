2e7ec41f5c0b27108bd697845cf4a46d52b6d9c0
owanemi <owanemi1741@gmail.com>
2025-04-24 17:04:18 +0100

    feat: added createWallet function

--- src/common/helpers/suiHelper.ts
+++ src/common/helpers/suiHelper.ts
@@ -1,20 +1,50 @@
-import * as sui from '@mysten/sui';
-import * as bip39 from 'bip39';
-
+import provider from '../utils/sui';
+import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
 import {
   BalancePayload,
   CreateWalletPayload,
+  GenerateWalletFromMnemonicPayload,
+  GetAddressFromPrivateKeyPayload,
+  GetTransactionPayload,
+  GetWalletFromEncryptedjsonPayload,
+  IGetTokenInfoPayload,
+  ISmartContractCallPayload,
+  ITokenInfo,
+  IResponse,
   TransferPayload,
 } from '../utils/types';
+import * as bip39 from 'bip39';
+import { successResponse } from '../utils';
 
-const createWallet = ({ derivationPath }: CreateWalletPayload) => {
+
+
+const getConnection = (rpcUrl?: string) => {
+  const connection = provider(rpcUrl);
+
+  return connection;
+};
+
+const createWallet = ({ derivationPath }: CreateWalletPayload): IResponse => {
+  // default ed25519 derivation path for sui
   const path = derivationPath || "m/44'/784'/0'/0'/0'";
-  const mnemonic = bip39.generateMnemonic();
-  const seed = bip39.mnemonicToSeedSync(mnemonic);
 
-  console.log(sui); // Example usage to avoid unused import error
+  const mnemonic = bip39.generateMnemonic();
+
+  const keypair = Ed25519Keypair.deriveKeypair(mnemonic, path);
+
+  const publicKey = keypair.getPublicKey();
+  const address = publicKey.toSuiAddress();
+
+  // get the secret key(Bech32-encoded, sui standard)
+  const secretKey = keypair.getSecretKey();
+
+  return successResponse({
+    address,
+    privateKey: secretKey,
+    mnemonic,
+  });
 };
 
 export default {
-  createWallet,
-};
+  createWallet
+}
