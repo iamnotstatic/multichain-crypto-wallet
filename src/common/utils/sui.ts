496d3dad1330a24e6e5a5df261a3a1821f10cd82
owanemi <owanemi1741@gmail.com>
2025-04-25 15:45:11 +0100

    chore: format with prettier

--- src/common/utils/sui.ts
+++ src/common/utils/sui.ts
@@ -2,7 +2,7 @@ import { SuiClient } from '@mysten/sui/client';
 
 const provider = (rpcUrl?: string) => {
   return new SuiClient({
-    url: rpcUrl as string
+    url: rpcUrl as string,
   });
 };
 
