bbd6702404fda390261aff6b3bb4fa90fa0b2c62
owanemi <owanemi1741@gmail.com>
2025-04-29 15:59:58 +0100

    fix ESM module test errors

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,28 @@
 const config = {
-  testTimeout: 100000, 
+  testTimeout: 100000,
   moduleNameMapper: {
     '^axios$': 'axios/dist/axios.js',
   },
   globals: {
     'ts-jest': {
       isolatedModules: true,
+      useESM: true,
     },
   },
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   setupFiles: ['./jest.setup.js'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': ['babel-jest', { 
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript'
+      ],
+    }]
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@mysten/sui|valibot)/)'
+  ],
 };
 
 module.exports = config;
